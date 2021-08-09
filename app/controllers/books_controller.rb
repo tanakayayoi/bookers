605e1abd329266fa4d79bcf67801a697747c366e
tanakayayoi <march.yayoi31@gmail.com>
2021-08-09 19:20:23 +0000

    [Fix]createアクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path(book.id)
   end
 
   def edit
